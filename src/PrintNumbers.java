2f6cd64b9e12de786fbdbe7cc27a75563506627c
Ashutosh Kumar <seleniumjan2021@gmail.com>
2024-12-04 17:15:34 +0530

    Added code for sum of two numbers

--- src/PrintNumbers.java
+++ src/PrintNumbers.java
@@ -1,12 +1,19 @@
 public class PrintNumbers {
 
     public static void main(String[] args) {
+
+
         printNumbers(8);
+        System.out.println( sumOfTwoNumbers(4,6));
     }
 
     private static void printNumbers(int num) {
         for (int i =1 ; i <=num;i++){
             System.out.println(i);
         }
+    }
+
+    public static int sumOfTwoNumbers(int a , int b){
+        return a+b;
     }
 }
