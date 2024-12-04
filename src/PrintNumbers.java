8d702209f6e767387531441545d695a7093bbc10
Baba Hansdah <babahansdah15@gmail.com>
2024-12-04 16:48:02 +0530

    First Commit

---
+++ src/PrintNumbers.java
@@ -0,0 +1,12 @@
+public class PrintNumbers {
+
+    public static void main(String[] args) {
+        printNumbers(8);
+    }
+
+    private static void printNumbers(int num) {
+        for (int i =1 ; i <=num;i++){
+            System.out.println(i);
+        }
+    }
+}
